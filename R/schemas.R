0af2c0c4c7de971e3f611211be1420b3cc58a053
Vince Carey <stvjc@channing.harvard.edu>
2022-10-01 10:51:22 -0400

    some fields to keep

---
+++ R/schemas.R
@@ -0,0 +1,28 @@
+#' FHIR Resource types recognized in package
+#' @export
+FHIR_ResourceTypes = function() {
+ c("AllergyIntolerance", "CarePlan", "CareTeam", 
+  "Claim", "Condition", "DiagnosticReport", 
+  "Encounter", "ExplanationOfBenefit", "Immunization", 
+  "MedicationRequest", "Observation", "Organization", 
+  "Patient", "Practitioner", "Procedure")
+}
+
+#' collection of FHIR Resource components to be retained
+#' @export
+FHIR_retention_schemas = function() {
+list(
+  Condition = c("id", "onsetDateTime", "code", "subject"),
+  AllergyIntolerance = c("id", "onsetDateTime", "code", "patient", "category"),
+  CarePlan = c("id", "activity", "subject", "category"),
+  Claim = c("id", "provider", "patient", "billablePeriod", "insurance", "created"),
+  Encounter = c("id", "type", "subject", "period", "serviceProvider"),
+  MedicationRequest = c("id", "subject", "status", "requester", "medicationCodeableConcept"),
+  Procedure = c("id", "subject", "status", "performedPeriod", "code"),
+  Patient = c("id", "identifier", "name", 
+    "telecom", "gender", "birthDate", "deceasedDateTime", "address", 
+    "maritalStatus", "multipleBirthBoolean", "communication", "active")
+  Immunization = c("id", "patient", "vaccineCode", "occurrenceDateTime")
+ )
+)
+
