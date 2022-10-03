8b8238cbf49baf23346e540c74053fe6c2e5df66
Vince Carey <stvjc@channing.harvard.edu>
2022-10-03 06:50:53 -0400

    revise app with about tab

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,21 +1,5 @@
-#process_CarePlan.R:process_CarePlan = function(CarePlan) {
-#process_Claim.R:process_Claim = function(Claim) {
-#process_Condition.R:process_Condition = function(Condition) {
-#process_Observation.R:process_Observation = function(Observation) {
-#processOthers.R:process_Encounter = function(Encounter) {
-#processOthers.R:process_AllergyIntolerance = function(AllergyIntolerance) {
-#processOthers.R:#process_Observation = function(Observation) {
-#processOthers.R:process_MedicationRequest = function(MedicationRequest) {
-#processOthers.R:process_Procedure = function(Procedure) {
-#processOthers.R:process_Immunization = function(Immunization) {
-#process_Patient.R:process_Patient = function(Patient) {
-#use_json.R:process_fhir_bundle = function(json_file) {
-
-
-#' table app
-#' @import shiny
-#' @export
-FHIRtabs = function() {
+library(shiny)
+library(BiocFHIR)
   fns = make_test_json_set()
   zn = gsub(".*jsontest/", "", fns)
   zns = strsplit(zn, "_")
@@ -24,7 +8,55 @@ FHIRtabs = function() {
   folks = paste(fn, ln, sep="_")
 
   names(fns) = folks
-  ui = fluidPage(
+  oldui = fluidPage(
+   sidebarLayout(
+    sidebarPanel(
+     helpText("FHIR viewer for synthea data subset"),
+     selectInput("indiv", "Person", choices=fns), width=2
+     ),
+    mainPanel(
+     tabsetPanel(
+      tabPanel("patient",
+       DT::dataTableOutput("patient")
+       ),
+      tabPanel("condition",
+       DT::dataTableOutput("condition")
+       ),
+      tabPanel("encounter",
+       DT::dataTableOutput("encounter")
+       ),
+      tabPanel("observation",
+       DT::dataTableOutput("observation")
+       ),
+      tabPanel("claims",
+       DT::dataTableOutput("claims")
+       )
+      )
+     )
+    )
+   ) # fluidPage
+
+   oldserver = function(input, output) {
+     fdat = reactive({
+      process_fhir_bundle(input$indiv)
+      })
+     output$patient = DT::renderDataTable({
+         process_Patient(fdat()$Patient)
+         })
+     output$observation = DT::renderDataTable({
+         process_Observation(fdat()$Observation)
+         })
+     output$claims = DT::renderDataTable({
+         process_Claim(fdat()$Claim)
+         })
+     output$condition = DT::renderDataTable({
+         process_Condition(fdat()$Condition)
+         })
+     output$encounter = DT::renderDataTable({
+         process_Encounter(fdat()$Encounter)
+         })
+     }
+ui = fluidPage(
    sidebarLayout(
     sidebarPanel(
      helpText("FHIR viewer for synthea data subset"),
@@ -97,6 +129,6 @@ an individual.  A 'fallback' schema usage may be adopted in future versions to c
          packageDescription("BiocFHIR")
          })
      }
-    runApp(list(ui=ui, server=server))
-   }
 
+
+shinyApp(ui=ui, server=server)
